0e2973fe4ae909e9c9abca1274157c6111a49d5c
César Terceiro <cbterceiro@gmail.com>
2019-05-27 19:38:25 -0300

    Criação do módulo e página de aniversariantes

---
+++ src/app/aniversariantes/aniversariantes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aniversariantes',
+  templateUrl: './aniversariantes.page.html',
+  styleUrls: ['./aniversariantes.page.scss'],
+})
+export class AniversariantesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
