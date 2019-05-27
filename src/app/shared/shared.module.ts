6ee70fdbfb6daaa5a6c1339bfe21a5f7e01bfe09
César Terceiro <cbterceiro@gmail.com>
2019-05-27 20:15:44 -0300

    Adição do FontAwesome ao projeto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   exports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    AngularFontAwesomeModule,
   ],
 })
 export class SharedModule { }
